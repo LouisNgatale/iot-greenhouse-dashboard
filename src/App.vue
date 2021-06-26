5f9caa5218ada3c57277fd9b6d99afa3699fba3f
Louis Ngatale <louisngatale@gmail.com>
2021-06-26 13:55:12 +0300

    UI Change

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
   <div id="app">
-    <div class="container">
-        <div class="row pt-2">
+    <div class="container-fluid">
+        <div class="row px-5 pt-2">
           <banner></banner>
         </div>
-        <div class="row">
-          <p class="title">Smart Greenhouse dashboard</p>
-        </div>
-        <div class="row">
-          <controls></controls>
+        <div class="row justify-content-start">
+			<div class="col-12 justify-content-start p-4 containers">
+				<div class="greetings">
+					<div class="image mb-2">
+						<img src="./assets/sunny.png">
+					</div>
+					<div class="mb-2">
+						Good morning,
+					</div>
+
+					<div class="mb-2">
+						Remember to irrigate the plants today for good growth.
+					</div>
+				</div>
+
+				<div class="widgets">
+					<div class="temperature">
+						<div class="image mb-2">
+							<img src="./assets/temperature.png">
+						</div>
+						<div class="sub-heading ">
+							Temperature
+						</div>
+
+						<div class="mb-2 temperature_value value">
+							22.5
+						</div>
+					</div>
+					<div class="humidity">
+						<div class="image mb-2">
+							<img src="./assets/humidity.png">
+						</div>
+						<div class="sub-heading">
+							Humidity
+						</div>
+
+						<div class="mb-2 humidity_value value">
+							80%
+						</div>
+					</div>
+					<div class="moisture">
+						<div class="image mb-2">
+							<img src="./assets/humidity.png">
+						</div>
+						<div class="sub-heading">
+							Soil moisture
+						</div>
+
+						<div class="mb-2 moisture_value value">
+							70%
+						</div>
+					</div>
+				</div>
+			</div>
         </div>
 
         <div class="row">
-          <div class="col m-2">
-            <humidity></humidity>
-          </div>
-          <div class="col m-2">
-            <temperature></temperature>
-          </div>
-          <div class="col m-2">
-            <moisture></moisture>
-          </div>
+			<div class="col-12 justify-content-start p-4 containers">
+				<div class="global temperature">
+					<div class="image mb-2">
+						<img src="./assets/temperature.png">
+					</div>
+					<div class="sub-heading ">
+						Outside Temperature
+					</div>
+
+					<div class="mb-2 temperature_value value">
+						22.5
+					</div>
+				</div>
+				<div class="global humidity">
+						<div class="image mb-2">
+							<img src="./assets/humidity.png">
+						</div>
+						<div class="sub-heading">
+							Outside Humidity
+						</div>
+
+						<div class="mb-2 humidity_value value">
+							80%
+						</div>
+				</div>
+			</div>
         </div>
     </div>
   </div>
@@ -56,14 +122,85 @@ export default {
 @import "./variables.scss ";
 
 #app{
-  background: #DBFFEA;
-  min-height: 100vh;
+	background: $background;
+	min-height: 100vh;
+	font-family: 'Ubuntu', sans-serif;
 
-  .title{
-    font-weight: bold;
-    font-size: 28px;
-    color: $bold-green;
-    margin-top: 16px;
-  }
+	.containers{
+		height:260px;
+		display: flex;
+	}
+	.value{
+		font-family: 'Raleway', sans-serif;
+		font-style: italic;
+		font-weight:700;
+		font-size: 90px;
+	}
+	.widgets{
+		display: flex;
+	}
+	.temperature_value{
+		color: $blue;
+	}
+	.humidity_value{
+		color: $orange;
+	}
+	.moisture_value{
+		color: $blue;
+	}
+	.widgets>*{
+		border-radius: 12px;
+		padding: 20px 21px;
+		margin:0 13px;
+		height: 100%;
+		background: $light_grey;
+		width: 300px;
+  		box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.2);
+	}
+
+	.sub-heading{
+		color: $grey;
+	}
+	
+	.global.temperature{
+		background: $dark;
+		width: 454px;
+		border-radius: 12px;
+		padding: 20px 21px;
+		margin:0 13px;
+		height: 100%;
+		color:white;
+  		box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.2);
+	}
+	
+	.global.humidity{
+		background: $light_grey;
+		width: 454px;
+		border-radius: 12px;
+		padding: 20px 21px;
+		margin:0 13px;
+		height: 100%;
+  		box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.2);
+	}
+	.global.temperature .sub-heading{
+		color: white;
+		font-weight: 400;
+	}
+	.greetings{
+		background: $dark;
+		margin:0 13px;
+		color:white;
+		width: 200px;
+  		box-shadow: 1px 3px 4px rgba(0, 0, 0, 0.2);
+		border-radius: 12px;
+		padding: 20px 21px;
+		height: 100%;
+	}
+	.title{
+		font-weight: bold;
+		font-size: 28px;
+		color: $bold-green;
+		margin-top: 16px;
+	}
 }
 </style>
